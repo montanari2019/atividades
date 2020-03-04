b763806e11f57b775eb7d75a80885289364ec88a
montanari2019 <ikaro.montanari31@gmail.com>
2020-03-03 20:47:52 -0400

    Conexão com o banco de dados realizada com sucesso

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://suporte:$uportE99@localhost/atividades'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+from app.models.tables import Usuario
+from app.models.tables import Atividade
+
 @app.route('/home')
 def home():
-    title = 'Atividades Complementares'
-    list = ['Oficina de Ads', 'Semana da Matemática']
-    return render_template('index.html', titulo=title, eventos = list)
+    title = 'Usuários'
+    usuarios = Usuario.query.all()
+    return render_template('index.html', titulo=title, user = usuarios)
